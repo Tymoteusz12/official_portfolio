fc8bc3ceb06781d6fdb2950b7be2f2b2af818046
Tymek Jagiello <tymekj23@interia.pl>
2020-09-05 16:05:29 +0200

    Updates

---
+++ precache-manifest.c58d361d712328c9401b3c239962d85c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93f8bea799703b5efaf3f64d5fba65de",
+    "url": "/official_portfolio/index.html"
+  },
+  {
+    "revision": "7117cf2db59a5eaf6db6",
+    "url": "/official_portfolio/static/css/main.eab24d3c.chunk.css"
+  },
+  {
+    "revision": "67a6b61e3c815ed25f6c",
+    "url": "/official_portfolio/static/js/2.180d86fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/official_portfolio/static/js/2.180d86fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7117cf2db59a5eaf6db6",
+    "url": "/official_portfolio/static/js/main.a80faab1.chunk.js"
+  },
+  {
+    "revision": "3016f49f1424bd327f3f",
+    "url": "/official_portfolio/static/js/runtime-main.48872efb.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/official_portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
